5e8403a895c942ad78c55b351b84498ee82de743
Ryan Collins <ryan@ohmgeek.co.uk>
2018-11-17 19:25:04 +0000

    Basic VNC works now

--- emulator_input_plugin/run.sh
+++ emulator_input_plugin/run.sh
@@ -1,5 +1,6 @@
 sleep 5;
-startx; /usr/games/mupen64plus --gfx mupen64plus-video-glide64mk2 --input /app/emulator_input_plugin/emulator_input_plugin.so /roms/mariokart.z64
+/opt/vnc.sh; 
+/usr/games/mupen64plus --gfx mupen64plus-video-glide64mk2 --input /app/emulator_input_plugin/emulator_input_plugin.so /roms/mariokart.z64
 
 #  --input /app/emulator_input_plugin/emulator_input_plugin.so
 # --gfx mupen64plus-video-glide64 
