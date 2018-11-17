91036e37e55e4127ddcb74f97ec4057af79abdc9
George Price <georgepriiice@gmail.com>
2018-11-17 16:33:23 +0000

    Input class testing

--- api/api_tests.py
+++ api/api_tests.py
@@ -1,6 +1,7 @@
 from controller import Controller
 from input import Input, Analog
 from input_types import InputTypes
+from random import random, randint
 import unittest
 
 
@@ -41,6 +42,73 @@ class APITest(unittest.TestCase):
         Testing that errors are raised when a fake button value is requested
         """
         self.assertRaises(ValueError, self._controller.get_button, "FAKE_BUTTON")
+
+    """
+    INPUT CONCRETE CLASS TESTING 
+    """
+
+    def test_input_constructor_inactive(self):
+        """
+        Tests that all the buttons in the controller default as inactive
+        """
+        self.assertTrue(all(map(lambda x: not self._controller.get_button(x).get_active(), InputTypes)))
+
+    def test_input_constructor_hold_time(self):
+        """
+        Tests that all the buttons in the controller default as hold time
+        """
+        self.assertTrue(all(map(lambda x: 0 == self._controller.get_button(x).get_held_time(), InputTypes)))
+
+    def test_input_set_hold_time(self):
+        """
+        Tests that the hold time for a button can be updated
+        """
+
+        # generate a list of random times to hold the buttons for
+        new_hold_times = [randint() for _ in range(len(InputTypes))]
+
+        # call set_active on these flips
+        for i, input_type in enumerate(InputTypes):
+            self._controller.get_button(input_type).set_active(new_hold_times[i])
+
+        # check that the ones with set_active called have been flipped by comparing to flips
+        self.assertEqual([self._controller.get_button(input_type).get_active() for input_type in InputTypes],
+                         new_hold_times)
+
+    def test_input_set_active(self):
+        """
+        Randomly select input types to flip, then check that their active state is flipped
+        """
+
+        # random flips
+        flips = [random() < 0.5 for _ in range(len(InputTypes))]
+
+        # call set_active on these flips
+        for i, input_type in enumerate(InputTypes):
+            self._controller.get_button(input_type).set_active(flips[i])
+
+        # check that the ones with set_active called have been flipped by comparing to flips
+        self.assertEqual([self._controller.get_button(input_type).get_active() for input_type in InputTypes],
+                         flips)
+
+    def test_input_set_held_time_invalid_negative(self):
+        """
+        Checking that negative values aren't allowed for new held-times for buttons
+        """
+        self.assertRaises(ValueError, self._controller.get_button(InputTypes.LEFT_ANALOG).set_hold_time(-1))
+
+    def test_input_set_held_time_invalid_type(self):
+        """
+        Checking that non-int and non-float values aren't allowed for new held-times for buttons
+        """
+        self.assertRaises(ValueError, self._controller.get_button(InputTypes.LEFT_ANALOG).set_hold_time("LONG TIME"))
+
+    def test_input_set_active_invalid(self):
+        """
+        Checking that non-boolean update values for the input types raise Exceptions
+        """
+        self.assertRaises(TypeError, self._controller.get_button(InputTypes.LEFT_TRIGGER).set_active('TRU'))
+
 
 
 if __name__ == "__main__":
