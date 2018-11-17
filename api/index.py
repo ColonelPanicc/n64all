6179c85d602124036fb89e6f2b90fc95b6f88913
JJ97 <johnmjennings97@gmail.com>
2018-11-17 15:41:41 +0000

    Starting post method + included requirements.txt

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 import hug
 from bee import *
+from input import Input, Analog
 from controller import Controller
 
 NUM_CONTROLLERS = 4
@@ -7,13 +8,26 @@ NUM_CONTROLLERS = 4
 controllers = {i : Controller() for i in range(NUM_CONTROLLERS)}
 
 @hug.get('/state', output=hug.output_format.json)
-def get_state(player_id:int=0):
+def get_state(player:int=0):
     try:
-        return controllers[player_id].get_state()
+        return controllers[player].get_state()
     except KeyError:
-        return {
-            'Error' : "That's not a valid player my dude"
-        }
+        return { 'Error' : "That's not a valid player my dude" }
+
+@hug.post('/update')
+def update(player:int=0, input:str="", active:bool=None, angle:float=None, tilt:float=None):
+    controller = controllers[player]
+    input = controller.get_button(input)
+
+    if isinstance(input, Analog):
+        if angle:
+            input.set_angle(angle)
+
+        if tilt:
+            input.set_tilt(tilt)
+
+    if active:
+        input.set_active(active)
 
 
 @hug.get('/test', output=hug.output_format.text)
