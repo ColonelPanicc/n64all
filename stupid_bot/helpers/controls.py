5fd8c20496984e536584b7a4a879a9509df5ed03
Ryan Collins <ryan@ohmgeek.co.uk>
2018-11-18 01:42:56 +0000

    Create random direction helper

--- stupid_bot/helpers/controls.py
+++ stupid_bot/helpers/controls.py
@@ -1,3 +1,6 @@
+import random
+
+
 class DirectionRandomGenerator:
     def __init__(self, min_X, max_X, min_Y, max_Y):
         self._min_X = min_X
@@ -5,3 +8,21 @@ class DirectionRandomGenerator:
 
         self._max_X = max_X
         self._max_Y = max_Y
+
+    def fetch_direction(self):
+        x = random.gauss(0, ((self._max_X) - (self._min_X)) / 2.0)
+
+        y = random.gauss(0, ((self._max_Y) - (self._min_Y)) / 2.0)
+
+        # edge checks
+        if(x < self._min_X):
+            x = self._min_X
+        elif (x > self._max_X):
+            x = self._max_X
+
+        if(y < self._min_Y):
+            y = self._min_Y
+        elif (y > self._max_Y):
+            y = self._max_Y
+
+        return (x, y)
