412e2adb61b7cb522387823f08d575edfe0bd2a8
JJ97 <johnmjennings97@gmail.com>
2018-11-18 01:00:06 +0000

    YEEEEEEEEEEEEEEEET

--- accelerometer/app.py
+++ accelerometer/app.py
@@ -3,17 +3,22 @@ import json
 import math
 import requests
 import threading
-
-def set_interval(func, sec):
-    def func_wrapper():
-        set_interval(func, sec)
-        func()
-    t = threading.Timer(sec, func_wrapper)
-    t.start()
-    return t
+import random
+#
+# def set_interval(func, sec):
+#     def func_wrapper():
+#         set_interval(func, sec)
+#         func()
+#     t = threading.Timer(sec, func_wrapper)
+#     t.start()
+#     return t
 
 PLAYER = "2"
 PLAYERS = 4
+X_TOLERANCE = 15
+
+global right
+right = 0
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'georgepricehasaf'
@@ -40,27 +45,36 @@ def login():
     return render_template('index.html')
 
 
+
+
 @app.route('/acc', methods=['POST'])
 def acc():
+    global right
     data = json.loads(request.data)
-    forward = data['gamma']
-    right = data['beta']
+    rt = data['alpha']
+
+    if -X_TOLERANCE <= rt <= X_TOLERANCE:
+        rt = 0
+
+    right += rt * 0.05
 
     right = min(127, max(-127, int(right)))
-    forward = min(127, max(-127, int(forward)))
-    state[PLAYER]["ANALOG"] = (right, forward)
-    state[PLAYER]["A_BTN"] = forward < 0
-    state[PLAYER]["B_BTN"] = forward > 0
+    state[PLAYER]["ANALOG"] = (right, 127)
+    state[PLAYER]["A_BTN"] = random.random() < 0.99
+    # state[PLAYER]["B_BTN"] = forward > 0
 
-    print('{} forward  {} right'.format(forward, right))
+    send_to_api()
+
+    # print('{} forward  {} right'.format(forward, right))
     return json.dumps(request.json)
 
 def send_to_api():
     url = "http://localhost:8000/update"
     data_string = json.dumps(state)
+    print(state)
     requests.post(url, data_string)
 
-set_interval(send_to_api, 0.25)
+# set_interval(send_to_api, 0.25)
 
 if __name__ == '__main__':
     print('hello')
