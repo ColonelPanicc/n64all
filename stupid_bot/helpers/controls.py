21f8138f44b377234628c663a682caa37bc110cd
Ryan Collins <ryan@ohmgeek.co.uk>
2018-11-18 01:06:11 +0000

    Added basic stupid bot boilerplate

---
+++ stupid_bot/helpers/controls.py
@@ -0,0 +1,7 @@
+class DirectionRandomGenerator:
+    def __init__(self, min_X, max_X, min_Y, max_Y):
+        self._min_X = min_X
+        self._min_Y = min_Y
+
+        self._max_X = max_X
+        self._max_Y = max_Y
