aeb95119f3b814d35935d4207b967476ba25abcd
Ryan Collins <ryan@ohmgeek.co.uk>
2019-10-19 14:45:03 +0100

    #18: Add state and tests for ControllerState

--- server/src/main/java/com/colonelpanic/n64all/server/model/ControllerState.java
+++ server/src/main/java/com/colonelpanic/n64all/server/model/ControllerState.java
@@ -2,10 +2,13 @@ package com.colonelpanic.n64all.server.model;
 
 import com.colonelpanic.n64all.server.service.PlayerController;
 import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class ControllerState implements PlayerController {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerState.class);
     private Map<ControllerProperty, Integer> properties;
 
     public ControllerState() {
