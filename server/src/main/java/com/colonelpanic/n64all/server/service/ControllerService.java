b9c06a620e26a548bb34cfc479f87f3ee204532f
Ryan Collins <ryan@ohmgeek.co.uk>
2019-10-19 14:31:25 +0100

    #18: add a basic command service

--- server/src/main/java/com/colonelpanic/n64all/server/service/ControllerService.java
+++ server/src/main/java/com/colonelpanic/n64all/server/service/ControllerService.java
@@ -1,8 +1,44 @@
 package com.colonelpanic.n64all.server.service;
 
+import com.colonelpanic.n64all.server.model.ControllerProperty;
+import com.colonelpanic.n64all.server.model.ControllerState;
+import com.google.common.collect.Maps;
+import com.google.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
+@Singleton
 public class ControllerService {
     private static final Logger logger = LoggerFactory.getLogger(ControllerService.class);
+    private Map<Integer, PlayerController> players;
+
+    public ControllerService() {
+        players = Maps.newConcurrentMap();
+    }
+
+    public void addPlayer(int playerId) {
+
+        if(players.containsKey(playerId))
+            return;
+
+        // TODO use a Guice supplied Factory to get the appropriate state.
+        players.put(playerId, new ControllerState());
+    }
+    public void updatePlayerState(int playerId, ControllerProperty propToUpdate, int valueToSet) {
+        if(this.players.get(playerId) == null) {
+            logger.error("Player with id: {} not found.", playerId);
+            throw new PlayerNotFoundException();
+        }
+        this.players.get(playerId).setState(propToUpdate, valueToSet);
+    }
+
+    public int getPlayerState(int playerId, ControllerProperty property) {
+        if(this.players.get(playerId) == null) {
+            logger.error("Player with id: {} not found.", playerId);
+            throw new PlayerNotFoundException();
+        }
+        return this.players.get(playerId).getState(property);
+    }
 }
